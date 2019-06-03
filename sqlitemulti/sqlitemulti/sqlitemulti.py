eb3ffba84e4c13c41234952b53095c2ae53cb1aa
EggPool <dev@eggpool.net>
2019-06-03 20:57:43 +0200

    First functional version and tests

--- sqlitemulti/sqlitemulti/sqlitemulti.py
+++ sqlitemulti/sqlitemulti/sqlitemulti.py
@@ -6,12 +6,12 @@ Main module.
 SqliteMulti classes, sqlite3 wrapped for threadsafe calls and serialization.
 """
 
-# from abc import ABC, abstractmethod
 import sqlite3
-from threading import Thread
+from threading import Thread, get_ident
 from queue import Queue
 from multiprocessing import Process, Manager
 from enum import Enum
+from time import time
 
 
 # When a queue is that old and empty, delete it
@@ -31,18 +31,17 @@ class SqlCommand(Enum):
 def sqlite_worker(
     queue,
     database,
-    timeout=None,
+    timeout=5,
     isolation_level=None,
-    factory=None,
     uri=None,
     verbose: bool = False,
 ):
     """Worker, running in thread or Process"""
     try:
-        db = sqlite3.connect(database, timeout, isolation_level, factory, uri)
+        db = sqlite3.connect(database, timeout=timeout, isolation_level=isolation_level, uri=uri)
     except Exception as e:
         if verbose:
-            print("DB Process error at connect {e}")
+            print(f"DB Process error at connect {e}")
         raise
     if verbose:
         print(f"DB Queue {database} started")
@@ -56,24 +55,39 @@ def sqlite_worker(
             res = None
             if command == SqlCommand.EXECUTE:
                 # Execute and sends None back
-                db.execute(sql, params)
+                if params:
+                    db.execute(sql, params)
+                else:
+                    db.execute(sql)
             elif command == SqlCommand.INSERT:
                 # Execute and sends inserted back
-                db.execute(sql, params)
+                if params:
+                    db.execute(sql, params)
+                else:
+                    db.execute(sql)
                 # TODO
                 # res =
             elif command == SqlCommand.DELETE:
                 # Execute and sends deleted back
-                db.execute(sql, params)
+                if params:
+                    db.execute(sql, params)
+                else:
+                    db.execute(sql)
                 # TODO
                 # res =
             elif command == SqlCommand.FETCHONE:
                 # Execute and sends the value back
-                result = db.execute(sql, params)
+                if params:
+                    result = db.execute(sql, params)
+                else:
+                    result = db.execute(sql)
                 res = result.fetchone()
             elif command == SqlCommand.FETCHALL:
                 # Execute and sends the value back
-                result = db.execute(sql, params)
+                if params:
+                    result = db.execute(sql, params)
+                else:
+                    result = db.execute(sql)
                 res = result.fetchall()
             elif command == SqlCommand.COMMIT:
                 db.commit()
@@ -91,25 +105,29 @@ def sqlite_worker(
             queue.task_done()
         except Exception as e:
             if verbose:
-                print("DB Process running {e}")
+                print(f"DB Process running {e}")
 
 
 class SqliteMulti:
     """Tries to mimic sqlite3 interface as much as possible"""
 
-    __slots__ = ("_command_queue", "_result_queues", "_worker")
+    __slots__ = ("_command_queue", "_own_process", "_result_queues", "_worker", "_verbose", "_stopping")
 
     def __init__(
         self,
         database,
-        timeout=None,
+        timeout=5,
         isolation_level=None,
-        factory=None,
         uri=None,
         own_process=False,
-        verbose: bool = False,
+        verbose: bool=False,
     ):
         self._result_queues = dict()
+        self._own_process = own_process
+        self._verbose = verbose
+        self._stopping = False
+        if verbose:
+            print("__Init__")
         if own_process:
             self._command_queue = Manager().Queue()
             self._worker = Process(
@@ -119,7 +137,6 @@ class SqliteMulti:
                     database,
                     timeout,
                     isolation_level,
-                    factory,
                     uri,
                     verbose,
                 ),
@@ -134,7 +151,6 @@ class SqliteMulti:
                     database,
                     timeout,
                     isolation_level,
-                    factory,
                     uri,
                     verbose,
                 ),
@@ -146,11 +162,87 @@ class SqliteMulti:
     def connect(
         cls,
         database,
-        timeout=None,
+        timeout=5,
         isolation_level=None,
-        factory=None,
         uri=None,
+        own_process=False,
         verbose: bool = False,
     ):
         """Alias to __init__, to be alike sqlite3 interface"""
-        return cls(database, timeout, isolation_level, factory, uri, verbose)
+        return cls(database, timeout, isolation_level, uri, own_process, verbose)
+
+    def status(self) -> str:
+        """Returns a status of current queues occupation"""
+        try:
+            status = f"{self._command_queue.qsize()} commands\n"
+        except:
+            # Note that this may raise NotImplementedError on Unix platforms like Mac OS X where sem_getvalue() is not implemented.
+            pass
+        status += f"{len(self._result_queues)} result queues\n"
+        try:
+            for id, queue in self._result_queues.items():
+                status += f"  {id}: {queue[0].qsize()}\n"
+        except:
+            pass
+        return status
+
+    def stop(self):
+        """Signal the worker to end"""
+        if self._verbose:
+            print("Stop required")
+        self._stopping = True
+        self._command_queue.put((None, SqlCommand.STOP, '', None, False))
+
+    def join(self):
+        """Waits until the worker ends nicely. only to be called after a stop(), or will never return"""
+        if self._verbose:
+            print("Join required")
+        if not self._stopping:
+            raise RuntimeError("Join was required, but no stop() before")
+        self._worker.join()
+
+    def _execute(self, command: SqlCommand, sql: str, params=None, commit=False):
+        """Generic queued command. Enqueues the request, and waits for the answer."""
+        thread_id = str(get_ident())
+        if self._verbose:
+            print(f"Called from thread {thread_id}")
+        if thread_id in self._result_queues:
+            # we had a queue, is it still good? - we need some garbage collector to delete old queues
+            print("TODO - check thread")
+            result_queue = self._result_queues[thread_id][0]
+        else:
+            if self._own_process:
+                result_queue = Manager().Queue()
+            else:
+                result_queue = Queue()
+            self._result_queues[thread_id] = (result_queue, time() + OLD_QUEUE_TRIGGER)
+        # Enqueue the command
+        self._command_queue.put((result_queue, command, sql, params, commit))
+        # And wait for its answer
+        if self._verbose:
+            print("Waiting...")
+        res = result_queue.get()
+        result_queue.task_done()
+        return res
+
+    def commit(self):
+        """Signal the worker to commit"""
+        return self._execute(SqlCommand.COMMIT, '')
+
+    def execute(self, sql, params=None, commit=False):
+        """Emulates an execute. Enqueues the request, and waits for the answer."""
+        return self._execute(SqlCommand.EXECUTE, sql, params, commit)
+
+    def fetchall(self, sql, params=None):
+        return self._execute(SqlCommand.FETCHALL, sql, params)
+
+    def fetchone(self, sql, params=None):
+        return self._execute(SqlCommand.FETCHONE, sql, params)
+
+    def insert(self, sql, params=None, commit=True):
+        """Emulates an insert. commit is True by default. Enqueues the request, and waits for the answer."""
+        return self._execute(SqlCommand.INSERT, sql, params, commit)
+
+    def delete(self, sql, params=None, commit=True):
+        """Emulates a delete. commit is True by default. Enqueues the request, and waits for the answer."""
+        return self._execute(SqlCommand.DELETE, sql, params, commit)
--- sqlitemulti/tests/test_sqlitemulti.py
+++ sqlitemulti/tests/test_sqlitemulti.py
@@ -1,25 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tests for `sqlitemulti` package."""
+"""Tests for `sqlitemulti` package.
+
+execute all tests by running "pytest -v" in the test repo, or launch each .py to get a more verbose output of a subset of tests only.
+"""
 
 import pytest
+import os
+import sys
+
+sys.path.append("../")
+from sqlitemulti.sqlitemulti import SqliteMulti
 
 
-from sqlitemulti import sqlitemulti
+# Create mempool table
+SQL_CREATE = "CREATE TABLE IF NOT EXISTS transactions (" \
+             "timestamp TEXT, address TEXT, recipient TEXT, amount TEXT, signature TEXT, " \
+             "public_key TEXT, operation TEXT, openfield TEXT, mergedts INTEGER)"
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_connect_db():
+    if os.path.isfile("test.db"):
+        os.remove("test.db")
+    db = SqliteMulti.connect("test.db")
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_create_table():
+    if os.path.isfile("test.db"):
+        os.remove("test.db")
+    db = SqliteMulti.connect("test.db")
+    db.execute(SQL_CREATE, commit=True)  # Will do sql + commit
+    # reread
+    res = db.fetchall("PRAGMA table_info('transactions')")
+    assert str(res) == "[(0, 'timestamp', 'TEXT', 0, None, 0), (1, 'address', 'TEXT', 0, None, 0), (2, 'recipient', 'TEXT', 0, None, 0), (3, 'amount', 'TEXT', 0, None, 0), (4, 'signature', 'TEXT', 0, None, 0), (5, 'public_key', 'TEXT', 0, None, 0), (6, 'operation', 'TEXT', 0, None, 0), (7, 'openfield', 'TEXT', 0, None, 0), (8, 'mergedts', 'INTEGER', 0, None, 0)]"
+
+
+if __name__ == "__main__":
+    if os.path.isfile("test.db"):
+        os.remove("test.db")
+    db = SqliteMulti.connect("test.db", verbose=True)
+    db.execute("PRAGMA journal_mode = WAL")
+    db.execute(SQL_CREATE, commit=True)  # Will do sql + commit
+    # reread
+    res = db.fetchall("PRAGMA table_info('transactions')")
+    print(res)
+    assert str(res) == "[(0, 'timestamp', 'TEXT', 0, None, 0), (1, 'address', 'TEXT', 0, None, 0), (2, 'recipient', 'TEXT', 0, None, 0), (3, 'amount', 'TEXT', 0, None, 0), (4, 'signature', 'TEXT', 0, None, 0), (5, 'public_key', 'TEXT', 0, None, 0), (6, 'operation', 'TEXT', 0, None, 0), (7, 'openfield', 'TEXT', 0, None, 0), (8, 'mergedts', 'INTEGER', 0, None, 0)]"
+
+    db.stop()
+    print(db.status())
+    # Wait for end
+    db.join()
